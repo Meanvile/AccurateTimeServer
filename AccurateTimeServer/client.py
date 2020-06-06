4466688e5877930112265fcf3099bc07d91b1af4
Meanvile <55655138+Meanvile@users.noreply.github.com>
2020-06-06 22:49:32 +0500

    Add files via upload

--- AccurateTimeServer/client.py
+++ AccurateTimeServer/client.py
@@ -1,14 +1,15 @@
 import socket
-import datetime
+from datetime import datetime
 
 s = socket.socket()
 s.connect(('127.0.0.1', 123))
+print("Клиент в сети...")
 while True:
     string = input("Нажмите любую клавишу: ")
     s.send(string.encode())
     if string == "quit":
         break
-    curr_time = datetime.datetime.now()
-    print('Настоящее время: ' + str(curr_time))
+    print('Настоящее время: ' + str(datetime.now()))
     print("Ложное время: ", s.recv(1024).decode())
+print("Завершение процесса...")
 s.close()
--- AccurateTimeServer/server.py
+++ AccurateTimeServer/server.py
@@ -1,16 +1,17 @@
 import socket
-import datetime
+from datetime import datetime, timedelta
 
 s = socket.socket()
 s.bind(('localhost', 123))
 s.listen(5)
+print("Cервер работает...")
 c, addr = s.accept()
 while True:
     recv = c.recv(4096).decode()
     if recv == "quit":
         break
     deceit_time = int(open('config.txt').read().strip())
-    curr_time = datetime.datetime.now()
-    callback = str((curr_time + datetime.timedelta(seconds=deceit_time)))
+    callback = str((datetime.now() + timedelta(seconds=deceit_time)))
     c.send(callback.encode())
+print('Произошло отключение сервера')
 s.close()
